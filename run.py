26635ebe9264dca5933b31c73a37df0e351645ad
Sanyam Kapoor <1sanyamkapoor@gmail.com>
2017-11-12 00:41:05 -0500

    Add test

--- run.py
+++ run.py
@@ -6,33 +6,63 @@ from torchvision.datasets.mnist import MNIST
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
+data_train = MNIST('./data/mnist',
+                   download=True,
+                   transform=transforms.Compose([
+                       transforms.Scale((32, 32)),
+                       transforms.ToTensor(),
+                   ]))
+data_test = MNIST('./data/mnist',
+                   train=False,
+                   download=True,
+                   transform=transforms.Compose([
+                       transforms.Scale((32, 32)),
+                       transforms.ToTensor(),
+                   ]))
+data_train_loader = DataLoader(data_train, batch_size=1024, shuffle=True, num_workers=8)
+data_test_loader = DataLoader(data_test, batch_size=1024, num_workers=8)
+
+net = LeNet5()
+criterion = nn.NLLLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.1)
+
+
+def train(epoch):
+    net.train()
+    for i, (images, labels) in enumerate(data_train_loader):
+        images, labels = Variable(images), Variable(labels)
+
+        optimizer.zero_grad()
+
+        output = net(images)
+
+        loss = criterion(output, labels)
+        print('Train - Epoch %d, Batch: %d, Loss: %f' % (epoch, i, loss.data[0]))
+
+        loss.backward()
+        optimizer.step()
+
+
+def test():
+    net.eval()
+    total_correct = 0
+    avg_loss = 0.0
+    for i, (images, labels) in enumerate(data_test_loader):
+        images, labels = Variable(images), Variable(labels)
+        output = net(images)
+        avg_loss += criterion(output, labels).sum()
+        pred = output.data.max(1)[1]
+        total_correct += pred.eq(labels.data.view_as(pred)).sum()
+
+    avg_loss /= len(data_test)
+    print('Test Avg. Loss: %f, Accuracy: %f' % (avg_loss.data[0], float(total_correct) / len(data_test)))
+
 
 def main():
-    data_train = MNIST('./data/mnist',
-                       download=True,
-                       transform=transforms.Compose([
-                           transforms.Scale((32, 32)),
-                           transforms.ToTensor(),
-                       ]))
-    data_train_loader = DataLoader(data_train, batch_size=1024, shuffle=True, num_workers=8)
+    for e in range(1, 11):
+        train(e)
 
-    net = LeNet5()
-    criterion = nn.NLLLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.1)
-
-    for e in range(10):
-        for i, (images, labels) in enumerate(data_train_loader):
-            images, labels = Variable(images), Variable(labels)
-
-            optimizer.zero_grad()
-
-            output = net(images)
-
-            loss = criterion(output, labels)
-            print('Epoch %d, Batch: %d, Loss: %f' % (e, i, loss.data[0]))
-
-            loss.backward()
-            optimizer.step()
+    test()
 
 
 if __name__ == '__main__':
