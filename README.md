98141313d9173348e417f8c8e3e6b5ab974e2180
Sanyam Kapoor <1sanyamkapoor@gmail.com>
2017-11-20 01:48:48 -0500

    Add visdom visualizations

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # LeNet-5
 
 This implements a slightly modified LeNet-5 and achieves an accuracy of ~99% on the MNIST dataset.
+
+## Setup
+
+Install all dependencies using the following command
+
+```
+$ pip install -r requirements.txt
+```
+
+## Usage
+
+Start the `visdom` server for visualization
+
+```
+$ python -m visdom.server
+```
+
+Start the training procedure
+
+```
+$ python run.py
+```
+
+See epoch train loss live graph at `[http://localhost:8097](http://localhost:8097)`.
--- run.py
+++ run.py
@@ -1,17 +1,21 @@
 from lenet import LeNet5
+import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
 from torchvision.datasets.mnist import MNIST
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
+import visdom
 
-data_train = MNIST('~/pytorch_data/mnist',
+viz = visdom.Visdom()
+
+data_train = MNIST('./pytorch_data/mnist',
                    download=True,
                    transform=transforms.Compose([
                        transforms.Scale((32, 32)),
                        transforms.ToTensor()]))
-data_test = MNIST('~/pytorch_data/mnist',
+data_test = MNIST('./pytorch_data/mnist',
                   train=False,
                   download=True,
                   transform=transforms.Compose([
@@ -24,9 +28,20 @@ net = LeNet5()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(net.parameters(), lr=2e-3)
 
+cur_batch_win = None
+cur_batch_win_opts = {
+    'title': 'Epoch Loss Trace',
+    'xlabel': 'Batch Number',
+    'ylabel': 'Loss',
+    'width': 1200,
+    'height': 600,
+}
+
 
 def train(epoch):
+    global cur_batch_win
     net.train()
+    loss_list, batch_list = [], []
     for i, (images, labels) in enumerate(data_train_loader):
         images, labels = Variable(images), Variable(labels)
 
@@ -35,8 +50,19 @@ def train(epoch):
         output = net(images)
 
         loss = criterion(output, labels)
+
+        loss_list.append(loss.data[0])
+        batch_list.append(i+1)
+
         if i % 10 == 0:
             print('Train - Epoch %d, Batch: %d, Loss: %f' % (epoch, i, loss.data[0]))
+
+        # Update Visualization
+        if viz.check_connection():
+            cur_batch_win = viz.line(torch.FloatTensor(loss_list), torch.FloatTensor(batch_list),
+                                     win=cur_batch_win, name='current_batch_loss',
+                                     update=(None if cur_batch_win is None else 'replace'),
+                                     opts=cur_batch_win_opts)
 
         loss.backward()
         optimizer.step()
