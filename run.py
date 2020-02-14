96902e137c4bbcc2bf531e4a933cef80d1ca22fd
Konstantin Luebeck <konstantin.luebeck@uni-tuebingen.de>
2020-02-14 15:46:13 +0100

    Added ONNX export.

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ from torchvision.datasets.mnist import MNIST
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 import visdom
+import onnx
 
 viz = visdom.Visdom()
 
@@ -83,9 +84,15 @@ def train_and_test(epoch):
     train(epoch)
     test()
 
+    dummy_input = torch.randn(1, 1, 32, 32, requires_grad=True)
+    torch.onnx.export(net, dummy_input, "lenet.onnx")
+
+    onnx_model = onnx.load("lenet.onnx")
+    onnx.checker.check_model(onnx_model)
+
 
 def main():
-    for e in range(1, 16):
+    for e in range(1, 3):
         train_and_test(e)
 
 
